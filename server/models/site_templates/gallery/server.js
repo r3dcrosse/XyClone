45df5623cb3fefcdfd33bbb8896cfc950c15cf22
David Wayman <mrdavidwayman@gmail.com>
2017-01-02 19:51:55 -0800

    Adds ejs support for templates on server

---
+++ server/models/site_templates/gallery/server.js
@@ -0,0 +1,45 @@
+// const path = require('path');
+const express = require('express');
+// const bodyParser = require('body-parser');
+// const router = require('./resources/routes');
+// const webpack = require('webpack');
+// const config = require('../webpack.config');
+
+// const mongoose = require('mongoose');
+// const morgan = require('morgan');
+//
+// const jwt = require('jsonwebtoken');
+// const database = require('./config');
+// const User = require('./models/user');
+//
+// const controller = require('./resources/controller')
+
+const app = express();
+// const compiler = webpack(config);
+
+
+//////////////////////////////
+// config ===================
+//////////////////////////////
+const port = process.env.PORT || 1337;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//////////////////////////////
+// routes ===================
+//////////////////////////////
+// app.get('*', function(req, res) {
+//   res.sendFile('index.html', {root: './'});
+// });
+
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+
+app.listen(port);
+console.log('Server is listening at port: ' + port);
+
+// export our app for testing and flexibility, required by index.js
+module.exports = app;
