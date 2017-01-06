bc801ce3bd807235d1a5aa40bbdabcd72a1d8e56
Fiona Chiang <ffchiangster@gmail.com>
2017-01-06 14:46:16 -0800

    changes to webpack config for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './src/index',
+    './src/Index',
     'react-hot-loader/patch'
   ],
   output: {
