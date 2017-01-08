8e8ad2e2db23f732958cac5020558f7e401e38f0
David Wayman <mrdavidwayman@gmail.com>
2017-01-07 17:17:02 -0800

    Adds material UI text fields for login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
 require("../../Basic.less");
@@ -8,11 +9,29 @@ class Login extends Component {
   render() {
     return (
       <div className="App">
-        <input placeholder='Username' />
-        <input placeholder='Password' />
-        <RaisedButton label="Login" primary="true" href="/dashboard">
-          {/* <Link to='/dashboard' /> */} {/* Maybe still need??? */}
-        </RaisedButton>
+        <div className="loginpage-field-container">
+          <div className="loginpage-username-field">
+            <TextField
+              hintText="What is your username?"
+              floatingLabelText="Username"
+            />
+          </div>
+          <div className="loginpage-password-field">
+            <TextField
+              hintText="Please enter your password"
+              floatingLabelText="Password"
+              type="password"
+            />
+          </div>
+          <div className="loginpage-login-button">
+            <RaisedButton
+              label="Login"
+              primary="true"
+              href="/dashboard">
+              {/* <Link to='/dashboard' /> */} {/* Maybe still need??? */}
+            </RaisedButton>
+          </div>
+        </div>
       </div>
     );
   }
