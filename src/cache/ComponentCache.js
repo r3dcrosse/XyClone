161c0f04a78a1b41401726e329606eda153ac895
David Wayman <mrdavidwayman@gmail.com>
2017-01-09 19:51:32 -0800

    Adds react-color picker to control body background css color

--- src/cache/ComponentCache.js
+++ src/cache/ComponentCache.js
@@ -145,7 +145,7 @@ export const storage = {
       justifyContent: 'center',
       position: 'relative',
       alignItems: 'center',
-      backgroundColor: 'blue',
+      backgroundColor: '#000000',
       marginLeft: '180px',
       padding: '0px',
       width: '70%',
