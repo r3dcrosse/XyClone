b5da817d1b0d35e8c7afb8c8d5b26f378d1ecfa8
Evan Chang <evanc12103832@ucla.edu>
2017-01-14 16:12:25 -0800

    Login fix for sessions

--- src/reducers/LoginReducer.js
+++ src/reducers/LoginReducer.js
@@ -1,10 +1,16 @@
 import { _components, storage } from '../cache/ComponentCache';
 
-const initialState = {
-  loginStatus: {}
-}
+const persistedLogin = sessionStorage.getItem('loginStatus') ?
+  {
+    loginStatus: JSON.parse(sessionStorage.getItem('loginStatus'))
+  }
+    :
+  {
+    loginStatus: {}
+  }
 
-export default function xycloneLogin (state = initialState, action) {
+
+export default function xycloneLogin (state = persistedLogin, action) {
   switch(action.type) {
     case 'LOGIN_USER':
       // console.log(action.userInfo, 'THIS IS ACTION.USERID');
